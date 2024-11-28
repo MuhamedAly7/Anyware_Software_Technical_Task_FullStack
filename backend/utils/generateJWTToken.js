0a83a10d7f3d3cfbd8872d4d0403f28c27d4ffcb
MuhamedAly7 <muhammedaly021@gmail.com>
2024-11-28 17:52:53 +0200

    customize token expiration

--- backend/utils/generateJWTToken.js
+++ backend/utils/generateJWTToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (payload) => {
   const token = await jwt.sign(payload, process.env.JWT_SECRET_KEY, {
-    expiresIn: "10m",
+    expiresIn: process.env.JWT_EXPIRATION_TIME,
   });
   return token;
 };
